21af9a5b285d1aee946f814e5546001e22138c7a
Ali Gasymov <alik0211alik@gmail.com>
2019-02-23 16:02:54 +0400

    Try deploy by branch name

--- .now/now.pull.sh
+++ .now/now.pull.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 set -e
 
-PULL_NUM=${CI_PULL_REQUEST#"https://github.com/alik0211/weather-know/pull/"}
+CLEAN_BRANCH_NAME=${CIRCLE_BRANCH//\//-};
 
 JSON=$(cat <<-EOF
 {
-  "version": 2,
-  "name": "weather-know-pull-$PULL_NUM",
-  "alias": ["weather-know-pull-$PULL_NUM.now.sh"],
-  "routes": [
-    {
-      "src": "^/static/(.*)",
-      "headers": { "cache-control": "s-maxage=31536000,immutable" },
-      "dest": "/static/$1"
-    },
-    { "src": "^/favicon.ico", "dest": "/favicon.ico" },
-    { "src": "^/asset-manifest.json", "dest": "/asset-manifest.json" },
-    { "src": "^/manifest.json", "dest": "/manifest.json" },
-    { "src": "^/precache-manifest.(.*)", "dest": "/precache-manifest.$1" },
-    {
-      "src": "^/service-worker.js",
-      "headers": { "cache-control": "s-maxage=0" },
-      "dest": "/service-worker.js"
-    },
-    {
-      "src": "^/(.*)",
-      "headers": { "cache-control": "s-maxage=0" },
-      "dest": "/index.html"
-    }
+  "name": "$CIRCLE_PROJECT_REPONAME-$CLEAN_BRANCH_NAME",
+  "alias": [
+    "$CIRCLE_PROJECT_REPONAME-$CLEAN_BRANCH_NAME.now.sh"
   ]
 }
 EOF
