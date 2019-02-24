4f0848b904072074a64b40e2d8df10226aa9ede7
Ali Gasymov <alik0211alik@gmail.com>
2019-02-24 09:54:30 +0400

    Add action for fetch forecast by coordinates

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
 import { combineReducers } from 'redux';
 
-export default combineReducers({});
+import forecast from './forecast';
+
+export default combineReducers({ forecast });
---
+++ src/ui/app/cnt-app.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { getForecastByCoord } from '../../actions/forecast';
+
+import App from './cmp-app';
+
+const mapStateToProps = (state, ownProps) => {
+  return {};
+};
+
+const mapDispatchToProps = {
+  getForecastByCoord
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
