1dc9387d534c3ab8e2a5e4fe34f622cb1085c85c
Maksym9669 <sychov@mail.usf.edu>
2019-02-26 14:21:14 -0500

    Added geolocation information about the user

--- src/index.js
+++ src/index.js
@@ -21,4 +21,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-d
--- src/ui/app/cmp-app.js
+++ src/ui/app/cmp-app.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
 
+let latitude = 0;
+let longitude = 0;
+
 class App extends Component {
+  // getGeoData() {
+  //   const url =
+  //     'https://api.openweathermap.org/data/2.5/weather?lat=50.431782&lon=30.516382&appid=e355c588a9911a706b20a89a2fd6f8e5';
+  //   // "http://131.247.210.6:8000/Olena";
+  //   fetch(url)
+  //     .then(data => {
+  //       return data.json();
+  //     })
+  //     .then(res => {
+  //       console.log(res);
+  //     });
+  // }
+
+  getGeoData() {
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      var crd = pos.coords;
+
+      console.log('Your current position is:');
+
+      console.log(`More or less ${crd.accuracy} meters.`);
+
+      latitude = crd.latitude;
+      longitude = crd.longitude;
+
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
   componentDidMount() {
     const { getForecastByCoord } = this.props;
 
+    this.getGeoData();
+
     getForecastByCoord({
-      lat: 35,
-      lon: 139,
+      lat: latitude,
+      lon: longitude,
     });
   }
   render() {
