d96a07c5ff83c3af2dcb3a46e7797fb9c7f4f481
Ali Gasymov <alik0211alik@gmail.com>
2019-03-01 20:10:47 +0400

    Add support redux devtools (#13)

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,12 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { apiMiddleware } from 'redux-api-middleware';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState = {}) {
+  let composeEnhancers = compose;
+
+  if (process.env.NODE_ENV === 'development') {
+    if ('__REDUX_DEVTOOLS_EXTENSION_COMPOSE__' in window) {
+      composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+    }
+  }
+
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, apiMiddleware)
+    composeEnhancers(applyMiddleware(thunk, apiMiddleware))
   );
 }
