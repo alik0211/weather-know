417bfd87e3ef5bdd8c731be3ad31323b0f68572c
Int_21h <yuskovss@gmail.com>
2020-01-28 17:00:10 +0400

    feat: add autocomplete

--- src/ui/app/components/cmp-search.js
+++ src/ui/app/components/cmp-search.js
@@ -8,10 +8,12 @@ export default class Search extends React.Component {
     this.state = {
       city: null,
       searchString: '',
+      autoComplete: null,
     };
   }
 
   handleChangeInput = event => {
+    this.autoComplete(event.target.value);
     this.setState({ searchString: event.target.value });
   };
 
@@ -28,10 +30,11 @@ export default class Search extends React.Component {
       this.setState({ searchString: city });
     }
   };
-
   handleClickButton = () => {
-    const { searchString } = this.state;
-    let result = new Set();
+    this.props.setLocation({ city: this.state.searchString });
+  };
+
+  autoComplete = searchString => {
     var params = {
       method: 'POST',
       contentType: 'application/json',
@@ -45,9 +48,6 @@ export default class Search extends React.Component {
         locations: [{ country: '*' }],
       }),
     };
-
-    this.props.setLocation({ city: this.state.searchString });
-
     fetch(`${DADATA_ROOT_API}`, params)
       .then(response => {
         return response.json();
@@ -58,10 +58,29 @@ export default class Search extends React.Component {
         });
       })
       .then(data => {
-        data.map(item => result.add(item.data.city || item.data.settlement));
-        console.log(result);
-        return result;
+        return data.filter(item => {
+          return (
+            (item.data.city &&
+              item.data.city
+                .toLowerCase()
+                .includes(searchString.toLowerCase())) ||
+            (item.data.settlement &&
+              item.data.settlement
+                .toLowerCase()
+                .includes(searchString.toLowerCase()))
+          );
+        });
+      })
+      .then(data => {
+        this.setState({ autoComplete: data });
       });
+  };
+
+  handleAutoComplete = searchString => {
+    this.setState({
+      searchString: searchString,
+      autoComplete: null,
+    });
   };
 
   componentDidUpdate() {
@@ -72,19 +91,37 @@ export default class Search extends React.Component {
   }
 
   render() {
-    const { searchString } = this.state;
+    const { searchString, autoComplete } = this.state;
     return (
-      <div className="search">
-        <input
-          className="search__input"
-          value={searchString}
-          onChange={this.handleChangeInput}
-          onFocus={this.handleFocusInput}
-          onBlur={this.handleBlurInput}
-        />
-        <button className="search__button" onClick={this.handleClickButton}>
-          search
-        </button>
+      <div>
+        <div className="search">
+          <input
+            className="search__input"
+            value={searchString}
+            onChange={this.handleChangeInput}
+            onFocus={this.handleFocusInput}
+            onBlur={this.handleBlurInput}
+          />
+          <button className="search__button" onClick={this.handleClickButton}>
+            search
+          </button>
+        </div>
+        {autoComplete &&
+          autoComplete.map(item => {
+            return (
+              <div
+                key={item.value}
+                className="search__list"
+                onClick={() =>
+                  this.handleAutoComplete(
+                    item.data.city || item.data.settlement
+                  )
+                }
+              >
+                {item.value}
+              </div>
+            );
+          })}
       </div>
     );
   }
