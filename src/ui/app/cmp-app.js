2374fb3ea64825bcbeee96769471e25a6c6f13b2
int <yuskovss@gmail.com>
2020-02-10 23:49:41 +0400

    tmp: Current component

--- src/ui/app/cmp-app.js
+++ src/ui/app/cmp-app.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import Search from '../search/cnt-search';
 
+import Current from '../current/cmp-current';
 import './app.css';
 
 class App extends Component {
   componentDidMount() {
     const { getLocation, getForecast } = this.props;
 
-    getLocation().then(() => {
-      getForecast();
-    });
+    // getLocation().then(() => {
+    //   getForecast();
+    // });
   }
 
   render() {
@@ -17,23 +18,29 @@ class App extends Component {
 
     return (
       <div className="app">
-        <Search />
-        {byTimestamp &&
-          Object.values(byTimestamp).map(item => {
-            return (
-              <div key={item.dt} className="forecast__day">
-                <div>
-                  data: {item.dt_txt.slice(0, item.dt_txt.indexOf(' '))}
-                </div>
-                <div>temp: {item.main.temp}</div>
-                <div>feels_like: {item.main.feels_like}</div>
-                <div>wind: {item.wind.speed}</div>
-                <div>weather: {item.weather.description}</div>
-              </div>
-            );
-          })}
+        <Current />
       </div>
     );
+
+    // return (
+    //   <div className="app">
+    //     <Search />
+    //     {byTimestamp &&
+    //       Object.values(byTimestamp).map(item => {
+    //         return (
+    //           <div key={item.dt} className="forecast__day">
+    //             <div>
+    //               data: {item.dt_txt.slice(0, item.dt_txt.indexOf(' '))}
+    //             </div>
+    //             <div>temp: {item.main.temp}</div>
+    //             <div>feels_like: {item.main.feels_like}</div>
+    //             <div>wind: {item.wind.speed}</div>
+    //             <div>weather: {item.weather.description}</div>
+    //           </div>
+    //         );
+    //       })}
+    //   </div>
+    // );
   }
 }
 
