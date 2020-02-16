8936faa854e4d64aef7f1d55ddfcadd429485391
int <yuskovss@gmail.com>
2020-02-16 23:44:53 +0400

    feat: add forecast day

--- src/reducers/forecast.js
+++ src/reducers/forecast.js
@@ -2,28 +2,80 @@ import { GET_FORECAST_SUCCESS } from '../actions/forecast';
 
 const initialState = {
   byTimestamp: {},
+  byDate: {},
   allTimestamp: [],
+  allDate: [],
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_FORECAST_SUCCESS:
       const { list } = action.payload;
+
       const forecastByDay = list.filter(item => item.dt_txt.includes('12:00'));
+      let dataSet = new Set();
+      let timeSet = new Set();
       return {
         ...state,
-        byTimestamp: {
-          ...forecastByDay.reduce((accumulator, item) => {
+        // byTimestamp: {
+        //   ...forecastByDay.reduce((accumulator, item) => {
+        //     return {
+        //       ...accumulator,
+        //       [item.dt]: item,
+        //     };
+        //   }, {}),
+        // },
+        // allTimestamp: forecastByDay.map(item => item.dt),
+        byDate: {
+          ...list.reduce((acc, item) => {
+            const date = item.dt_txt.slice(0, 10);
+            dataSet.add(date);
             return {
-              ...accumulator,
-              [item.dt]: item,
+              ...acc,
+              [date]: {
+                ...list
+                  .filter(item => item.dt_txt.includes(date))
+                  .reduce((acc, item) => {
+                    const time = item.dt_txt.slice(11);
+                    timeSet.add(time);
+                    return {
+                      ...acc,
+                      [time]: item,
+                    };
+                  }, {}),
+                allTime: Array.from(timeSet).sort(),
+              },
             };
           }, {}),
         },
-        allTimestamp: forecastByDay.map(item => item.dt),
+        allDate: Array.from(dataSet).slice(0, 5),
       };
 
     default:
       return state;
   }
 }
+
+// export default function(state = initialState, action) {
+//   switch (action.type) {
+//     case GET_FORECAST_SUCCESS:
+//       const { list } = action.payload;
+//       console.log(list)
+//       const forecastByDay = list.filter(item => item.dt_txt.includes('12:00'));
+//       return {
+//         ...state,
+//         byTimestamp: {
+//           ...forecastByDay.reduce((accumulator, item) => {
+//             return {
+//               ...accumulator,
+//               [item.dt]: item,
+//             };
+//           }, {}),
+//         },
+//         allTimestamp: forecastByDay.map(item => item.dt),
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
--- src/ui/forcast-day/cmp-forecast-day.js
+++ src/ui/forcast-day/cmp-forecast-day.js
@@ -1,21 +1,88 @@
 import React from 'react';
+import Swiper from 'react-id-swiper';
+import 'swiper/swiper.scss';
+
+import ForecastHour from '../forecast-hour/foreact-hour';
 
 import './forecast-day.scss';
 
-export default function ForecastDay(props) {
-  return (
-    <div className="forecast-day">
-      <div className="forecast-day__name">Monday</div>
-      <time className="forecast-day__date">12 Feb</time>
-      <div className="temp forecast-day__temp">
-        <span className="temp__value">+7</span>
-        <span className="temp__inut">&deg;</span>
+export default class ForecastDay extends React.Component {
+  monthName = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Oct',
+    'Dec',
+  ];
+  dayName = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  render() {
+    const { onClick, full = false, data, color } = this.props;
+    const className = full ? 'forecast-day-full' : 'forecast-day';
+    const time = data.allTime.includes('12:00:00')
+      ? '12:00:00'
+      : data[data.allTime[0]].dt_txt.slice(11);
+    const d = new Date(data[time].dt_txt);
+    const month = this.monthName[d.getMonth()];
+    const date = d.getDate() < 10 ? `0${d.getDate()}` : `${d.getDate()}`;
+    const day = this.dayName[d.getDay()];
+
+    const params = {
+      slidesPerView: 'auto',
+      spaceBetween: 10,
+      freeMode: true,
+    };
+
+    return (
+      <div
+        className={className}
+        style={{ background: color }}
+        onClick={onClick}
+      >
+        <div className={`${className}__name`}>{day}</div>
+        <time className={`${className}__date`}>{`${date} ${month}`}</time>
+        <div className={`temp ${className}__temp`}>
+          <span className="temp__value">
+            {Math.round(data[time].main.temp)}
+          </span>
+          <span className="temp__inut">&deg;</span>
+        </div>
+        <img
+          className={`${className}__icon`}
+          src={require('./ModSnowSwrsDay.png')}
+          alt="cloud"
+        />
+        {full && (
+          <div className="forecast-hours-container">
+            {/* <Swiper {...params}>
+              {data.allTime.map(item => {
+                return (
+                  <div className="forecast-hour" key={item}>
+                    <ForecastHour
+                      data={data[item]}
+                    />
+                  </div>
+                )
+              })}
+            </Swiper> */}
+          </div>
+        )}
       </div>
-      <img
-        className="forecast-day__icon"
-        src={require('./ModSnowSwrsDay.png')}
-        alt="cloud"
-      />
-    </div>
-  );
+    );
+  }
 }
